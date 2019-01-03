4c94f2368826fe188b8a14fd5b027ff7bdb32749
Le Chinh <ledinhchinhit@gmail.com>
2019-01-03 14:31:26 +0700

    new logic

--- Calculator/ViewController/ViewController.swift
+++ Calculator/ViewController/ViewController.swift
@@ -2,232 +2,130 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lblTextLabel: UILabel!
-    var oldString: String?
-    var newString: String?
-    var flag = false
-    var flag2 = false
-    var phepTinh: String?
+    @IBOutlet weak private var textLabel: UILabel!
+    private var oldNumber: String?
+    private var newNumber: String?
+    private var isCalculatorButtonTapped = false
+    private var isFloatingPoint = false
+    private var calculation: String?
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
     
-    enum Calculation {
-        case cong
-        case tru
-        case nhan
-        case chia
-        case chiaPhanTram
+    enum CalculationType {
+        case add
+        case sub
+        case multi
+        case div
+        case percent
     }
     
-    func calculate(phepTinh: Calculation) {
-        switch phepTinh {
-        case .cong:
-            newString = lblTextLabel.text
-            if flag2 {
-                let result: Double = Double(oldString!)! + Double(newString!)!
-                lblTextLabel.text = String(result)
-                flag2 = false
-            } else {
-                let result: Int = Int(oldString!)! + Int(newString!)!
-                lblTextLabel.text = String(result)
-            }
-        case .tru:
-            newString = lblTextLabel.text
-            if flag2 {
-                let result: Double = Double(oldString!)! - Double(newString!)!
-                lblTextLabel.text = String(result)
-                flag2 = false
-            } else {
-                let result: Int = Int(oldString!)! - Int(newString!)!
-                lblTextLabel.text = String(result)
-            }
-        case .nhan:
-            newString = lblTextLabel.text
-            if flag2 {
-                let result: Double = Double(oldString!)! * Double(newString!)!
-                lblTextLabel.text = String(result)
-                flag2 = false
-            } else {
-                let result: Int = Int(oldString!)! * Int(newString!)!
-                lblTextLabel.text = String(result)
-            }
-        case .chia:
-            newString = lblTextLabel.text
-            let result: Double = Double(oldString!)! / Double(newString!)!
-            lblTextLabel.text = String(result)
-        case .chiaPhanTram:
-            let result: Double = Double(oldString!)! / 100
-            lblTextLabel.text = String(result)
+    func calculate(calculation: CalculationType) {
+        newNumber = textLabel.text
+        guard let newNumbers = textLabel.text,
+              let newNumber = Double(newNumbers),
+              let oldNumbers = oldNumber,
+              let oldNumber = Double(oldNumbers) else {
+            fatalError("newNumber is nil")
+        }
+        var result: Double = 0
+        switch calculation {
+        case .add:
+            result = oldNumber + newNumber
+        case .sub:
+            result = oldNumber - newNumber
+        case .multi:
+            result = oldNumber * newNumber
+        case .div:
+            result = oldNumber / newNumber
+        case .percent:
+            result = oldNumber / 100
+            textLabel.text = String(result)
+            return
+        }
+        
+        if !isFloatingPoint {
+            textLabel.text = String(Int(result))
+        } else {
+            textLabel.text = String(result)
+            isFloatingPoint = false
         }
     }
     
     @IBAction func btnNumber(_ sender: UIButton) {
+        guard var currentLabel = textLabel.text else {
+            fatalError("")
+        }
         switch sender.tag {
         case 0:
-            if lblTextLabel.text == "0" {
+            if textLabel.text == "0" {
                 return
             } else {
-                lblTextLabel.text = lblTextLabel.text! + "0"
+                textLabel.text = currentLabel + "0"
             }
-        case 1:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "1"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "1"
-            }
-        case 2:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
+        case 1...9:
+            if isCalculatorButtonTapped {
+                textLabel.text = ""
+                currentLabel = ""
+                isCalculatorButtonTapped = false
             }
             
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "2"
+            if textLabel.text == "0" {
+                textLabel.text = ""
+                currentLabel = ""
+                textLabel.text = currentLabel + String(sender.tag)
             } else {
-                lblTextLabel.text = lblTextLabel.text! + "2"
-            }
-        case 3:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "3"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "3"
-            }
-        case 4:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "4"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "4"
-            }
-        case 5:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "5"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "5"
-            }
-        case 6:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "6"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "6"
-            }
-        case 7:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "7"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "7"
-            }
-        case 8:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "8"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "8"
-            }
-        case 9:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
-            }
-            
-            if lblTextLabel.text == "0" {
-                lblTextLabel.text = ""
-                lblTextLabel.text = lblTextLabel.text! + "9"
-            } else {
-                lblTextLabel.text = lblTextLabel.text! + "9"
+                textLabel.text = currentLabel + String(sender.tag)
             }
         case 11:
-            if flag {
-                lblTextLabel.text = ""
-                flag = false
+            if isCalculatorButtonTapped {
+                textLabel.text = ""
+                isCalculatorButtonTapped = false
             }
-            lblTextLabel.text = lblTextLabel.text! + "."
-            flag2 = true
+            textLabel.text = currentLabel + "."
+            isFloatingPoint = true
         case 12:
-            switch phepTinh {
+            switch calculation {
                 case "+":
-                    calculate(phepTinh: ViewController.Calculation.cong)
+                    calculate(calculation: ViewController.CalculationType.add)
                 case "-":
-                    calculate(phepTinh: ViewController.Calculation.tru)
+                    calculate(calculation: ViewController.CalculationType.sub)
                 case "*":
-                    calculate(phepTinh: ViewController.Calculation.nhan)
+                    calculate(calculation: ViewController.CalculationType.multi)
                 case "/":
-                    calculate(phepTinh: ViewController.Calculation.chia)
+                    calculate(calculation: ViewController.CalculationType.div)
                 default:
                     print("Khong co phep tinh nao")
             }
         case 13:
-            flag = true
-            phepTinh = "+"
-            oldString = lblTextLabel.text
+            isCalculatorButtonTapped = true
+            calculation = "+"
+            oldNumber = textLabel.text
         case 14:
-            flag = true
-            phepTinh = "-"
-            oldString = lblTextLabel.text
+            isCalculatorButtonTapped = true
+            calculation = "-"
+            oldNumber = textLabel.text
         case 15:
-            flag = true
-            phepTinh = "*"
-            oldString = lblTextLabel.text
+            isCalculatorButtonTapped = true
+            calculation = "*"
+            oldNumber = textLabel.text
         case 16:
-            flag = true
-            phepTinh = "/"
-            oldString = lblTextLabel.text
+            isCalculatorButtonTapped = true
+            calculation = "/"
+            oldNumber = textLabel.text
         case 17: 
-            flag = true
-            phepTinh = "%"
-            oldString = lblTextLabel.text
-            calculate(phepTinh: ViewController.Calculation.chiaPhanTram)
-        case 18:
-            lblTextLabel.text = lblTextLabel.text! + "2"
+            isCalculatorButtonTapped = true
+            calculation = "%"
+            oldNumber = textLabel.text
+            calculate(calculation: ViewController.CalculationType.percent)
         case 19:
-            lblTextLabel.text = "0"
+            textLabel.text = "0"
         default:
             print("Ko tim thay tag nao phu hop")
         }
     }
-    
 }
+
+
 
